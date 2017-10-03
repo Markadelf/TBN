2afc7d3a41bc0ba8cea2a38c8ebbe429ed0e8e5a
jxs2828 <jxs2828@IGM2000-16>
2017-10-02 22:08:55 -0400

    Added and started a GamepadController class

---
+++ TBN/Gameplay/Input/GamepadController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace TBN.Gameplay.Input
+{
+    class GamepadController : InputController
+    {
+
+
+        public GamepadController(int gamePadIndex)
+        {
+            GamePadCapabilities gamepad = GamePad.GetCapabilities(gamePadIndex);
+
+            //Ill need to talk with someone about how to implement this
+        }
+
+        public override bool ButtonHeld(int HistoryLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ButtonPressed(int HistoryLocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemapControls()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
