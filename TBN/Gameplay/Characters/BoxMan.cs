141b850aca7bbfbdd1ac0e0b562536118a5e93db
Mark <Mxd5700@rit.edu>
2018-01-02 00:57:12 -0500

    Minor Adjustments to more apropriatly reflect the desired end result.

--- TBN/Gameplay/Characters/BoxMan.cs
+++ TBN/Gameplay/Characters/BoxMan.cs
@@ -26,10 +26,39 @@ namespace TBN
 
         public BoxMan(Vector2 anchor, InputController input, SpriteSheet sheet) : base(anchor, input, sheet)
         {
+            MoveList["Idle"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["Idle"].EndFrame = 2;
 
+            MoveList["Walk"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["Walk"].EndFrame = 2;
 
+            MoveList["BackWalk"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["BackWalk"].EndFrame = 2;
 
+            MoveList["Crouch"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["Crouch"].EndFrame = 2;
 
+            MoveList["Precrouch"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["Precrouch"].EndFrame = 2;
+
+            MoveList["CrouchGetup"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["CrouchGetup"].EndFrame = 2;
+
+            MoveList["PreJump"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["PreJump"].EndFrame = 2;
+
+            MoveList["JumpIdle"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["JumpIdle"].EndFrame = 2;
+
+            MoveList["Jump"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["Jump"].EndFrame = 2;
+
+            MoveList["Attack"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["Attack"].AddHurtboxKeyFrame(0, new Rectangle[] { new Rectangle(16, -32, 32, 32) }, new float[] { 1 });
+            MoveList["Attack"].EndFrame = 2;
+            MoveList["Attack"].MaxHits = 1;
+            MoveList["Attack"].StunOnHit = 30;
+            MoveList["Attack"].StunOnBlock = 5;
 
         }
     }
--- TBN/Gameplay/Characters/StandardCharacter.cs
+++ TBN/Gameplay/Characters/StandardCharacter.cs
@@ -16,64 +16,38 @@ namespace TBN
         {
             //Define States
             MoveList.Add("Idle", new Action(0));
-            MoveList["Idle"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
-            MoveList["Idle"].EndFrame = 2;
             MoveList["Idle"].MyProperties = ActionProperties.Loops | ActionProperties.AllowBlock | ActionProperties.AllowGrab;
 
             MoveList.Add("Walk", new Action(4));
-            MoveList["Walk"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
             MoveList["Walk"].AddDisplacementKeyFrame(0, new Vector2(8, 0));
-            MoveList["Walk"].EndFrame = 2;
             MoveList["Walk"].MyProperties = ActionProperties.Loops | ActionProperties.AllowBlock | ActionProperties.AllowGrab;
 
             MoveList.Add("BackWalk", new Action(5));
-            MoveList["BackWalk"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
             MoveList["BackWalk"].AddDisplacementKeyFrame(0, new Vector2(-8, 0));
-            MoveList["BackWalk"].EndFrame = 2;
             MoveList["BackWalk"].MyProperties = ActionProperties.Loops | ActionProperties.AllowBlock | ActionProperties.AllowGrab;
 
             MoveList.Add("Crouch", new Action(2));
-            MoveList["Crouch"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
-            MoveList["Crouch"].EndFrame = 2;
             MoveList["Crouch"].MyProperties = ActionProperties.Loops | ActionProperties.AllowBlock | ActionProperties.AllowGrab;
 
             MoveList.Add("Precrouch", new Action(1));
-            MoveList["Precrouch"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
-            MoveList["Precrouch"].EndFrame = 2;
 
             MoveList.Add("CrouchGetup", new Action(3));
-            MoveList["CrouchGetup"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
-            MoveList["CrouchGetup"].EndFrame = 2;
 
             MoveList.Add("PreJump", new Action(6));
-            MoveList["PreJump"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
             MoveList["PreJump"].MiscBehaviors = new List<Tuple<int, SimpleBehavior>> { new Tuple<int, SimpleBehavior>(0, Behavior.SetJumpHeight(this, 120)) };
-            MoveList["PreJump"].EndFrame = 2;
 
             MoveList.Add("JumpIdle", new Action(8));
-            MoveList["JumpIdle"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
-            MoveList["JumpIdle"].EndFrame = 2;
             MoveList["JumpIdle"].MyProperties = ActionProperties.Loops | ActionProperties.AllowBlock | ActionProperties.AllowGrab;
 
             MoveList.Add("Jump", new Action(7));
-            MoveList["Jump"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
             MoveList["Jump"].MiscBehaviors = new List<Tuple<int, SimpleBehavior>> { new Tuple<int, SimpleBehavior>(-1, Behavior.OffGround(this)), new Tuple<int, SimpleBehavior>(-1, Behavior.FreeMoveX(this, 6)) };
-            MoveList["Jump"].EndFrame = 2;
             MoveList["Jump"].AddDisplacementKeyFrame(0, new Vector2(0, -5));
             MoveList["Jump"].MyProperties = ActionProperties.Loops | ActionProperties.AllowBlock | ActionProperties.AllowGrab;
 
             MoveList.Add("HitStun", new Action(1));
             MoveList["HitStun"].MyProperties = ActionProperties.AllowGrab;
 
-
             MoveList.Add("Attack", new Action(0));
-            MoveList["Attack"].AddHitboxKeyFrame(0, new Rectangle[] { new Rectangle(-16, -32, 32, 32) }, new float[] { 1 });
-            MoveList["Attack"].AddHurtboxKeyFrame(0, new Rectangle[] { new Rectangle(16, -32, 32, 32) }, new float[] { 1 });
-            MoveList["Attack"].EndFrame = 2;
-            MoveList["Attack"].MaxHits = 1;
-            MoveList["Attack"].StunOnHit = 30;
-            MoveList["Attack"].StunOnBlock = 5;
-
 
 
             //Connections
