8513025f3971882329ea5fd1f85fa614e2fb0506
Mark <Mxd5700@rit.edu>
2018-02-12 12:05:17 -0500

    Gamepad should work. Requires testing.

--- TBN/Gameplay/Input/GamepadController.cs
+++ TBN/Gameplay/Input/GamepadController.cs
@@ -10,23 +10,31 @@ namespace TBN
 {
     class GamepadController : InputController
     {
-
+        /// <summary>
+        /// reads entries as left, down, right, up, light punch, medium punch, heavy punch, special, short, roundhouse,  
+        /// </summary>
+        Buttons[] InpCodes;
+        float x;
+        float y;
+        GamePadState previous;
+        GamePadState current;
+        int index;
 
         public GamepadController(int gamePadIndex)
         {
             GamePadCapabilities gamepad = GamePad.GetCapabilities(gamePadIndex);
-
+            index = gamePadIndex;
             //Ill need to talk with someone about how to implement this
         }
 
         public override bool ButtonHeld(int HistoryLocation)
         {
-            return false;
+            return current.IsButtonDown(InpCodes[HistoryLocation]);
         }
 
         public override bool ButtonPressed(int HistoryLocation)
         {
-            return false;
+            return current.IsButtonDown(InpCodes[HistoryLocation]) && previous.IsButtonUp(InpCodes[HistoryLocation]);
         }
 
         public override void RemapControls()
@@ -36,7 +44,26 @@ namespace TBN
 
         public override void Update()
         {
-            //throw new NotImplementedException();
+            current = GamePad.GetState(index);
+            StickPos = current.ThumbSticks.Left;
+
+            for (int i = 0; i < InputHistory.Length; i++)
+            {
+                if (i < 9)
+                {
+                    InputHistory[i]++;
+                    int stickindex = (int)(4 + StickPos.X + 3 * StickPos.Y);
+                    InputHistory[stickindex] = 0;
+
+                }
+                else
+                {
+                    InputHistory[i] = (ButtonPressed(i)) ? //is the key pressed 
+                    0 : //if so set it to 0 
+                    InputHistory[i] + 1;//if not add one to the value if not it remains at 60
+                }
+            }
+            previous = current;
         }
     }
 }
